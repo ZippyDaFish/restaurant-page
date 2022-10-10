cc4fd201f8a3246c6f8d5caedcac2c56ffc20375
ZippyDaFish <andrew.riker21@gmail.com>
2022-10-10 15:32:30 -0400

    Begin transferring page creation to JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 console.log("Everything seems to be working");
+
+const content = document.getElementById('content');
+
+const headerMod = (() => {
+    const createHeader = () => {
+        const div = document.createElement("div");
+        div.setAttribute('id', "header");
+        div.setAttribute('class', "flex-container");
+        div.innerHTML = `
+        <p class="text-greater">Oops, All Soups</p>
+        <nav>
+            <button class="button-nav text-main">Home</button>
+            <button class="button-nav text-main">Menu</button>
+            <button class="button-nav text-main">Contact</button>
+        </nav>
+        `;
+        content.appendChild(div);
+        console.log("no blockage");
+    };
+    return[createHeader];
+})();
+
+headerMod.createHeader();
